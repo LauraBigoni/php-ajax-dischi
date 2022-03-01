f5b390a7b33af9f1fcb616f4504c0bf64b81b921
Laura Bigoni <laurabigoni.92@gmail.com>
2022-03-01 15:20:05 +0100

    milestone 1

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+
+$database = [
+    [
+        'title' => 'New Jersey',
+        'author' => 'Bon Jovi',
+        'year' => 1988,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+        'genre' => 'Rock'
+    ],
+    [
+        'title' => 'Live at Wembley 86',
+        'author' => 'Queen',
+        'year' => 1992,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Ten\'s Summoner\'s Tales',
+        'author' => 'Sting',
+        'year' => 1993,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Steve Gadd band',
+        'author' => 'Steve Gadd Band',
+        'year' => 2018,
+        'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+        'genre' => 'Jazz'
+    ],
+    [
+        'title' => 'Brave new World',
+        'author' => 'Iron Maiden',
+        'year' => 2000,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+        'genre' => 'Metal'
+    ],
+    [
+        'title' => 'One more car, one more rider',
+        'author' => 'Eric Clapton',
+        'year' => 2002,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+        'genre' => 'Rock'
+    ]
+];
+// var_dump($database);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Top Of the Pops</title>
+</head>
+
+<body>
+    <header>
+        <div class="row m-0">
+            <div class="col d-flex justify-content-center align-items-center">
+                <i class="fab fa-spotify" role="button"></i>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container albums">
+            <div class="row">
+                <div class="col  d-flex flex-wrap justify-content-around align-items-start">
+                    <?php $i = 1;
+                    foreach ($database as $result) { ?>
+                        <ul id="<?= $i ?>" class="list-unstyled">
+                            <li class="d-flex flex-column justify-content-center align-items-center">
+                                <img src="<?= $result['poster']; ?>" alt="<?= $result['author']; ?>">
+                                <h5 class="text-center"><?= $result['title']; ?></h5>
+                                <p><?= $result['author']; ?></p>
+                                <p><?= $result['year']; ?></p>
+                            </li>
+                        </ul>
+                    <?php $i++;
+                    } ?>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="row m-0">
+            <div class="col">
+                <nav class="d-flex justify-content-center align-items-center p-4">
+                    <ul class="pagination">
+                        <li class="page-item disabled">
+                            <a class="page-link" href="#">Previous</a>
+                        </li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">Next</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ traccia.md
@@ -0,0 +1,14 @@
+Stampare a schermo una decina di dischi musicali.
+
+Utilizzare:
+Html, CSS, VueJS, axios, PHP
+
+# Prima Milestone:
+Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+
+# Seconda Milestone:
+Attraverso l’utilizzo di axios:
+al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi  e li stamperà attraverso vue.
+
+# Bonus:
+Attraverso un’altra chiamata api, filtrare gli album per genere
